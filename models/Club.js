cfc414e0167bf8a6416dcec6f30dd7e61f187d38
Liam Mccluskey <liammail100@gmail.com>
2021-02-10 18:52:57 -0500

    updated club schema

--- models/Club.js
+++ models/Club.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 const ClubSchema = mongoose.Schema({
     name: {
         type: String,
-        required: true,
-        unique: true
+        required: true
     },
     adminUIDs: {
         type: [String],
