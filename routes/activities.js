4c2aa2d1f5506f59e46ee357a978aec3032f63b2
Liam Mccluskey <liammail100@gmail.com>
2021-03-27 15:25:52 -0400

    updating past activty query

--- routes/activities.js
+++ routes/activities.js
@@ -5,13 +5,41 @@ const Session = require('../models/Session')
 
 // PATH: /activities
 
+router.get('/test', async (req, res) => {
+    const reservedKeys = ['page', 'pagesize', 'sortby']
+    const filterQuery = Object.fromEntries(
+        Object.entries(req.query).filter(e => 
+            !reservedKeys.includes(e[0])
+        )
+    )
+
+    try {
+        const activities = await Activity.find(filterQuery)
+        .sort(req.query.sortby)
+
+        res.json(activities)
+    } catch (error) {
+        res.status(500).json({message: error})
+    }
+})
+
 // GET: all a users complted activities
 /*
     - results are paginated
-    - currently no query filter supported
+    - supported sort filters:
+        - date, distance, time, pace (asc, desc)
+    - supported query filters:
+        - distance (<, =, >), time (<,=,>), workoutType (=)
 */
 router.get('/uid/:uid', async (req, res) => {
     const pageSize = Math.min(50, req.query.pagesize)
+
+    const reservedKeys = ['page', 'pagesize', 'sortby']
+    const filterQuery = Object.fromEntries(
+        Object.entries(req.query).filter(e => 
+            !reservedKeys.includes(e[0])
+        )
+    )
     async function fetchSession(sessionID) {
         try {
             const session = Session.findById(sessionID)
@@ -25,13 +53,16 @@ router.get('/uid/:uid', async (req, res) => {
     }
     try {
         const activitiesCount = await Activity.find({
-            uid: req.params.uid
+            uid: req.params.uid,
+            ...filterQuery
         })
         .countDocuments()
 
         const activities = await Activity.find({
-            uid: req.params.uid
+            uid: req.params.uid,
+            ...filterQuery
         })
+        .sort(req.query.sortby)
         .skip( (req.query.page - 1) * pageSize)
         .limit(pageSize)
         .select('distance elapsedTime workoutType workoutItemIndex sessionID createdAt')
