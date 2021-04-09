482573bd364f89d4da01522f9eafe4d87bc1b867
Liam Mccluskey <liammail100@gmail.com>
2021-04-09 09:33:00 -0400

    added select iconURL

--- routes/clubs.js
+++ routes/clubs.js
@@ -68,14 +68,14 @@ router.get('/customURL/:customURL', async (req, res) => {
         club.members =  await User.find({
             uid: {$in: club.memberUIDs}
         })
-        .select('displayName')
+        .select('displayName iconURL')
         .sort('displayName')
         .lean()
 
         club.admins = await User.find({
             uid: {$in: club.adminUIDs}
         })
-        .select('displayName')
+        .select('displayName iconURL')
         .sort('displayName')
         .lean()
 
